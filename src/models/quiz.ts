beab692241058aef422c2524dd9bc5486aa32af2
innneang <innneang@Innns-MacBook-Pro.local>
2017-07-30 15:03:12 +0700

    Quiz Model, Feature

---
+++ src/models/quiz.ts
@@ -0,0 +1,11 @@
+export class QuizModel{
+
+	public name:string;
+	public pic:string;
+	public caption:string;
+	constructor(name:string, pic:string, caption:string){
+		this.name = name;
+		this.pic = pic;
+		this.caption = caption;
+	}
+}
--- src/pages/features/features.ts
+++ src/pages/features/features.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 
-import {QuizPage} from '../quiz/quiz';
+import { QuizPage } from '../quiz/quiz';
 
+import { QuizModel } from '../../models/quiz';
 /**
  * Generated class for the FeaturesPage page.
  *
@@ -11,21 +12,28 @@ import {QuizPage} from '../quiz/quiz';
  */
 
 @Component({
-    selector: 'page-features',
-    templateUrl: 'features.html',
+  selector: 'page-features',
+  templateUrl: 'features.html',
 })
 export class FeaturesPage {
+  public quiznames: any;
+  Quizs=[
+    new QuizModel("Bird", 'Bird', "Bird Quiz Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies"),
+    new QuizModel("Fish", 'Peacock', "Peacok Quiz Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies"),
+    new QuizModel("Fuck", 'Sunflower', "Sunflower Quiz Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies"),
+    ];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+  }
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-    }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeaturesPage');
+  }
+featurespage(quiz: any){
+  this.navCtrl.push(QuizPage, {
+    quiz: quiz,
+  });
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad FeaturesPage');
-    }
+}
 
-    featurespage() {
-
-        this.navCtrl.push(QuizPage);
-
-    }
 }
