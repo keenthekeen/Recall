721c6b6fcd6f43bc51278f305b44e19df345a81e
innneang <innneang@Innns-MacBook-Pro.local>
2017-07-31 15:34:29 +0700

    camera added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+//import { ImagePicker } from '@ionic-native/image-picker';
+import { Camera } from '@ionic-native/camera';
 
 import { FeaturesPage } from '../pages/features/features';
 import { MyApp } from './app.component';
@@ -35,6 +37,8 @@ import { AddquizPage } from  '../pages/addquiz/addquiz';
   providers: [
     StatusBar,
     SplashScreen,
+   // ImagePicker,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/addquiz/addquiz.ts
+++ src/pages/addquiz/addquiz.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Camera, CameraOptions } from '@ionic-native/camera';
 /**
  * Generated class for the AddquizPage page.
  *
@@ -12,13 +12,36 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-addquiz',
   templateUrl: 'addquiz.html',
 })
+
 export class AddquizPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+
   }
+
+
+  	getpicture(){
+  		console.log('runned');
+  		const options: CameraOptions = {
+		  quality: 100,
+		  destinationType: this.camera.DestinationType.DATA_URL,
+		  encodingType: this.camera.EncodingType.JPEG,
+		  mediaType: this.camera.MediaType.PICTURE,
+		  sourceType: 0,
+		}
+
+  		this.camera.getPicture(options).then((imageData) => {
+		 // imageData is either a base64 encoded string or a file URI
+		 // If it's base64:
+		 let base64Image = 'data:image/jpeg;base64,' + imageData;
+		}, (err) => {
+		 // Handle error
+		});
+  	}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddquizPage');
   }
+
 
 }
