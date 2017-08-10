c2fae2b73e371c3133997c75d6061f9a370d0084
Sarat Limawongpranee <37404@bodin.ac.th>
2017-08-10 23:24:25 +0700

    Revert "Save prepare for revert"
    This reverts commit a6819c7fa9224f724900984223b644da3ff03510.

--- src/pages/features/features.ts
+++ src/pages/features/features.ts
@@ -1,4 +1,4 @@
-import {Component,ViewChild,ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 
 import {QuizPage} from '../quiz/quiz';
@@ -20,37 +20,18 @@ import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database
 export class FeaturesPage {
     public quizzes: FirebaseListObservable<any[]>;
 
-    @ViewChild('imagex') img: ElementRef;
-
-    constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase, templateElement: ElementRef) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase) {
         this.quizzes = QuizModel.fetch(db);
-        //let img = templateElement.getElementById('imagex') as HTMLImageElement;
-        console.log(this.img)
-        //img.src ='https://www.thewrap.com/wp-content/uploads/2015/11/Donald-Trump.jpg';
-
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad FeaturesPage');
-        console.log('img', this.img);
-        this.img.nativeElement.src = 'https://www.thewrap.com/wp-content/uploads/2015/11/Donald-Trump.jpg';
-
     }
 
     quizPage(quiz: any) {
         this.navCtrl.push(QuizPage, {
             quiz: quiz
         });
-
-    }
-    loadImg(){
-        let _img = <HTMLImageElement>document.getElementById('imagex');
-        _img = document.createElement('img');
-        let newImg = new Image;
-        newImg.src = 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi';
-        newImg.onload = function() {
-            _img.src = newImg.src;
-        }
 
     }
 
